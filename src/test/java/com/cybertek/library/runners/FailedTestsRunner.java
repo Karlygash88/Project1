8d38355fcf3b6678fa00c1b3dd170615bf1aa269
Karlygash88 <karlygash.ospan@gmail.com>
2021-03-22 16:09:29 -0700

    Updated pom.xml

---
+++ src/test/java/com/cybertek/library/runners/FailedTestsRunner.java
@@ -0,0 +1,15 @@
+package com.cybertek.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src/test/resources/features",
+        glue = "rerun:target/rerun.txt"
+
+)
+public class FailedTestsRunner {
+}
